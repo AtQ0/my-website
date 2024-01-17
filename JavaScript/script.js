9c3598531f8e7a9bec1670be306956d6c893915b
AtQ0 <mail@atkobabic.com>
2024-01-17 20:01:13 +0100

    onhover social icons
    put scale on hover of social icons

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -65,9 +65,9 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 
-/*==========================================================*/
-/*======= APPEARANCE/DISSAPEARANCE OF NAV ON SCROLL ========*/
-/*==========================================================*/
+/*=================================================*/
+/*=== APPEARANCE/DISSAPEARANCE OF NAV ON SCROLL ===*/
+/*=================================================*/
 
 
 
@@ -141,7 +141,7 @@ function onFirstHamburgerClick() {
     //Initiate opacity transition from 0 to 0.7
     setTimeout(function () {
         localOverlayDiv.classList.add("increase-overlay-opacity");
-    }, 500)
+    }, 300)
 
     //Compensate vertical pos of rectB if js onHover is not activated yet
     //It activates when user the first time hovers in/out of hamburger wrapper
